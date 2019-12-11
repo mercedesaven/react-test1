0e6c802b2304e7556c5312a6a240451834697114
mercedesaven <mercedes.aven@gmail.com>
2019-12-10 22:19:04 -0300

    first commit

---
+++ src/Apptest.js
@@ -0,0 +1,25 @@
+import React from 'react';
+//import {BrowserRouter as Router, Route } from 'react-router-dom' 
+
+
+function Apptest() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://ichef.bbci.co.uk/news/410/cpsprodpb/15665/production/_107435678_perro1.jpg' className="App-logo" alt="logo" />
+        <p>Hola aca todo bien. 
+        </p>
+        <a
+          className="App-link"
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Entra a facebook
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Apptest;
